9e9f4741bd054691194667b62753386891d9c4a9
Chuluq <choirulchuluq10@gmail.com>
2025-12-03 13:42:24 +0700

    page information

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
 
-	let { children, data } = $props();
+	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<p>{data.pathname}</p>
+<p>{page.url.pathname}</p>
 
 <ul>
 	<li><a href="/">Home</a></li>
--- src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-import type { LoadEvent } from '@sveltejs/kit';
-
-export function load({ url }: LoadEvent) {
-	return {
-		pathname: url.pathname
-	};
-}
