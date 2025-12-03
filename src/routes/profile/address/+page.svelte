adc7dc9508f9afa67cbfee79283354cb938ee310
Chuluq <choirulchuluq10@gmail.com>
2025-12-03 09:47:45 +0700

    Load data

--- src/routes/profile/address/+page.svelte
+++ src/routes/profile/address/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+	const { data } = $props();
+</script>
+
 <h1>Address</h1>
+<p>{data.user}</p>
---
+++ src/routes/profile/user/+page.ts
@@ -0,0 +1,9 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = () => {
+	return {
+		user: 'chuluq',
+		name: 'Moch Chaerul Chuluq',
+		email: 'chuluq@test.com'
+	};
+};
