ba8c0ae94861ad4fd38f56ad33332d1cb4f93d29
Chuluq <choirulchuluq10@gmail.com>
2025-12-04 14:14:34 +0700

    server load parameter

---
+++ src/routes/users/dashboard/+page.server.ts
@@ -0,0 +1,16 @@
+import { redirect, type RequestEvent } from '@sveltejs/kit';
+
+export function load({ cookies, url }: RequestEvent) {
+	if (url.searchParams.get('logout')) {
+		cookies.delete('user', { path: '/' });
+		redirect(303, '/users/login');
+	}
+
+	if (!cookies.get('user')) {
+		redirect(303, '/users/login');
+	}
+
+	return {
+		user: cookies.get('user')
+	};
+}
---
+++ src/routes/users/login/+page.svelte
@@ -0,0 +1,5 @@
+<form action="/users/login" method="get">
+	<label for="user">User</label>
+	<input type="text" name="user" id="user" />
+	<button>Login</button>
+</form>
