86846a3c51c9608e738d20e4ca57001e402b684e
Chuluq <choirulchuluq10@gmail.com>
2025-12-07 09:33:38 +0700

    error

---
+++ src/routes/(default)/products/[id]/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1>Error: {page.status}</h1>
+{#if page.error}
+	<p>Message: {page.error.message}</p>
+{/if}
--- src/routes/(default)/products/[id]/+page.ts
+++ src/routes/(default)/products/[id]/+page.ts
@@ -1,6 +1,11 @@
-import type { LoadEvent } from '@sveltejs/kit';
+import { error, type LoadEvent } from '@sveltejs/kit';
 
 export async function load({ params, fetch }: LoadEvent) {
 	const response = await fetch(`/api/products/${params.id}.json`);
+
+	if (response.status !== 200) {
+		error(404, { message: 'Product not found' });
+	}
+
 	return await response.json();
 }
